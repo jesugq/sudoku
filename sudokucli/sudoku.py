c9c232d2b7e6fa46902c192e72be109bebb1e369
jesugq <jesugq@gmail.com>
2021-01-14 12:13:01 -0600

    Add sudoku correct checking, tests

--- sudokucli/sudoku.py
+++ sudokucli/sudoku.py
@@ -1,3 +1,92 @@
+from typing import List
 class Sudoku:
     def __init__(self):
-        print()
+        self.ROW = 0
+        self.COL = 1
+        self.LEN = 9
+        self.board = [[]]
+        self.empty_board()
+    
+    def empty_board(self) -> List[List]:
+        self.board = [[0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0]]
+
+    def change_board(self, board: List[List]) -> List[List]:
+        self.board = board
+
+    def verify_pos(self, pos: int) -> bool:
+        return pos >= 0 and pos < self.LEN
+
+    def convert_box_row(self, pos: int) -> List:
+        if pos in [0, 1, 2]:
+            return range(0, 3)
+        elif pos in [3, 4, 5]:
+            return range(3, 6)
+        elif pos in [6, 7, 8]:
+            return range(6, 9)
+        else:
+            return range(0)
+        
+    def convert_box_col(self, pos: int) -> List:
+        if pos in [0, 3, 6]:
+            return range(0, 3)
+        elif pos in [1, 4, 7]:
+            return range(3, 6)
+        elif pos in [2, 5, 8]:
+            return range(6, 9)
+        else:
+            return range(0)
+
+    def edit_board(self, number: int, position: List) -> None:
+        row = position[self.ROW]
+        col = position[self.COL]
+        self.board[row][col] = number
+
+    def correct_row(self, row: int) -> bool:
+        if not self.verify_pos(row):
+            return False
+
+        seen = []
+        for i in range(self.LEN):
+            value = self.board[row][i]
+            if value not in seen:
+                seen.append(value)
+            else:
+                return False
+        return True
+
+    def correct_col(self, col: int) -> bool:
+        if not self.verify_pos(col):
+            return False
+
+        seen = []
+        for i in range(self.LEN):
+            value = self.board[i][col]
+            if value not in seen:
+                seen.append(value)
+            else:
+                return False
+        return True
+
+    def correct_box(self, box: int) -> bool:
+        if not self.verify_pos(box):
+            return False
+        
+        seen = []
+        range_row = self.convert_box_row(box)
+        range_col = self.convert_box_col(box)
+        for i in range_row:
+            for j in range_col:
+                value = self.board[i][j]
+                if value not in seen:
+                    seen.append(value)
+                else:
+                    return False
+        return True
--- test/test_sudoku.py
+++ test/test_sudoku.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import List
 from ..sudokucli.sudoku import Sudoku
 
 @pytest.fixture
@@ -6,8 +7,201 @@ def sudoku_class():
     sudoku = Sudoku()
     return sudoku
 
-def test_sudoku_class(sudoku_class):
+@pytest.fixture
+def sudoku_empty_board():
+    return [[0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]]
+
+@pytest.fixture
+def sudoku_edited_board():
+    return [[9,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]]
+
+@pytest.fixture
+def sudoku_easy_board():
+    return [[0,6,0,3,0,0,7,4,0],
+            [5,3,7,0,9,0,0,0,0],
+            [0,4,0,0,0,6,3,0,7],
+            [0,9,0,0,5,1,2,3,8],
+            [0,0,0,0,0,0,0,0,0],
+            [7,1,3,6,2,0,0,4,0],
+            [3,0,6,4,0,0,0,1,0],
+            [0,0,0,0,6,0,5,2,3],
+            [1,0,2,0,0,9,0,8,0]]
+
+@pytest.fixture
+def sudoku_correct_row_board():
+    return [[1,2,3,4,5,6,7,8,9],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]]
+
+@pytest.fixture
+def sudoku_incorrect_row_board():
+    return [[1,2,3,1,5,9,7,8,9],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]]
+
+@pytest.fixture
+def sudoku_correct_col_board():
+    return [[1,0,0,0,0,0,0,0,0],
+            [2,0,0,0,0,0,0,0,0],
+            [3,0,0,0,0,0,0,0,0],
+            [4,0,0,0,0,0,0,0,0],
+            [5,0,0,0,0,0,0,0,0],
+            [6,0,0,0,0,0,0,0,0],
+            [7,0,0,0,0,0,0,0,0],
+            [8,0,0,0,0,0,0,0,0],
+            [9,0,0,0,0,0,0,0,0]]
+
+@pytest.fixture
+def sudoku_incorrect_col_board():
+    return [[1,0,0,0,0,0,0,0,0],
+            [2,0,0,0,0,0,0,0,0],
+            [3,0,0,0,0,0,0,0,0],
+            [1,0,0,0,0,0,0,0,0],
+            [5,0,0,0,0,0,0,0,0],
+            [9,0,0,0,0,0,0,0,0],
+            [7,0,0,0,0,0,0,0,0],
+            [8,0,0,0,0,0,0,0,0],
+            [9,0,0,0,0,0,0,0,0]]
+
+@pytest.fixture
+def sudoku_correct_box_board():
+    return [[1,2,3,0,0,0,0,0,0],
+            [4,5,6,0,0,0,0,0,0],
+            [7,8,9,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]]
+
+@pytest.fixture
+def sudoku_incorrect_box_board():
+    return [[1,2,3,0,0,0,0,0,0],
+            [1,5,9,0,0,0,0,0,0],
+            [7,8,9,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]]
+
+def test_sudoku_class_initialized(sudoku_class):
     if sudoku_class:
         assert sudoku_class
     else:
         assert False
+
+def test_sudoku_class_has_empty_board(sudoku_class, sudoku_empty_board):
+    assert sudoku_class.board == sudoku_empty_board
+
+def test_sudoku_class_changes_easy_board(sudoku_class, sudoku_easy_board):
+    sudoku_class.change_board(sudoku_easy_board)
+    assert sudoku_class.board == sudoku_easy_board
+
+def test_sudoku_class_edits_board_at_position(sudoku_class, sudoku_edited_board):
+    number: int = 9
+    position: List = [0,0]
+    sudoku_class.edit_board(9, position)
+    assert sudoku_class.board == sudoku_edited_board
+
+def test_sudoku_class_verifies_correct_pos(sudoku_class):
+    low_boundary: int = 0
+    high_boundary: int = 8
+    assert sudoku_class.verify_pos(low_boundary)
+    assert sudoku_class.verify_pos(high_boundary)
+
+def test_sudoku_class_verifies_incorrect_pos(sudoku_class):
+    low_broken: int = -1
+    high_broken: int = 9
+    assert not sudoku_class.verify_pos(low_broken)
+    assert not sudoku_class.verify_pos(high_broken)
+
+def test_sudoku_class_converts_correct_box_row(sudoku_class):
+    low_boundary: int = 0
+    high_boundary: int = 8
+    low_range: List = range(0, 3)
+    high_range: List = range(6, 9)
+    assert sudoku_class.convert_box_row(low_boundary) == low_range
+    assert sudoku_class.convert_box_row(high_boundary) == high_range
+
+def test_sudoku_class_converts_incorrect_box_row(sudoku_class):
+    low_broken: int = -1
+    high_broken: int = 9
+    broken_range: List = range(0)
+    assert sudoku_class.convert_box_row(low_broken) == broken_range
+    assert sudoku_class.convert_box_row(high_broken) == broken_range
+
+def test_sudoku_class_converts_correct_box_col(sudoku_class):
+    low_boundary: int = 0
+    high_boundary: int = 8
+    low_range: List = range(0, 3)
+    high_range: List = range(6, 9)
+    assert sudoku_class.convert_box_row(low_boundary) == low_range
+    assert sudoku_class.convert_box_row(high_boundary) == high_range
+
+def test_sudoku_class_converts_incorrect_box_col(sudoku_class):
+    low_broken: int = -1
+    high_broken: int = 9
+    broken_range: List = range(0)
+    assert sudoku_class.convert_box_col(low_broken) == broken_range
+    assert sudoku_class.convert_box_col(high_broken) == broken_range
+
+def test_sudoku_class_correct_row(sudoku_class, sudoku_correct_row_board):
+    row = 0
+    sudoku_class.change_board(sudoku_correct_row_board)
+    assert sudoku_class.correct_row(row)
+
+def test_sudoku_class_incorrect_row(sudoku_class, sudoku_incorrect_row_board):
+    row = 0
+    sudoku_class.change_board(sudoku_incorrect_row_board)
+    assert not sudoku_class.correct_row(row)
+
+def test_sudoku_class_correct_col(sudoku_class, sudoku_correct_col_board):
+    col = 0
+    sudoku_class.change_board(sudoku_correct_col_board)
+    assert sudoku_class.correct_col(col)
+
+def test_sudoku_class_incorrect_col(sudoku_class, sudoku_incorrect_col_board):
+    col = 0
+    sudoku_class.change_board(sudoku_incorrect_col_board)
+    assert not sudoku_class.correct_col(col)
+
+def test_sudoku_class_correct_box(sudoku_class, sudoku_correct_box_board):
+    box = 0
+    sudoku_class.change_board(sudoku_correct_box_board)
+    assert sudoku_class.correct_box(box)
+
+def test_sudoku_class_incorrect_box(sudoku_class, sudoku_incorrect_box_board):
+    box = 0
+    sudoku_class.change_board(sudoku_incorrect_box_board)
+    assert not sudoku_class.correct_box(box)
