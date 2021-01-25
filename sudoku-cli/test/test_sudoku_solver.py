795e99749e398f6a58aa091aab3b82b5ca5f3480
jesugq <jesugq@gmail.com>
2021-01-24 18:47:00 -0600

    Add missing tests from sudoku solver module

--- sudoku-cli/test/test_sudoku_solver.py
+++ sudoku-cli/test/test_sudoku_solver.py
@@ -3,9 +3,9 @@ from typing import List
 from sudoku_solver import SudokuSolver
 
 @pytest.fixture
-def sudoku():
+def default_sudoku():
     sudoku = SudokuSolver()
     return sudoku
 
-def test_sudoku_initialized(sudoku):
-    assert sudoku
+def test_sudoku_initialized(default_sudoku):
+    assert default_sudoku
