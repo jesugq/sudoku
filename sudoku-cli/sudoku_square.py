cdfa9b8747c9485545ff8ed1aec00b6f197653e2
jesugq <jesugq@gmail.com>
2021-01-30 09:24:07 -0600

    Add reset function and changes, tests

--- sudoku-cli/sudoku_square.py
+++ sudoku-cli/sudoku_square.py
@@ -1,20 +1,18 @@
 from typing import List
 
 class SudokuSquare:
-    def __init__(self, row: int, col: int, potentials: List[int] = []):
+    def __init__(self, row: int, col: int, potential: List[int] = []):
         self.row: int = row
         self.col: int = col
         self.pindex: int = -1
         self.number: int = 0
-        self.solved: bool = False
-        self.potentials: List = potentials
+        self.potential: List = potential
 
     def lock_number(self) -> None:
-        self.number = self.potentials[self.pindex]
-        self.solved = True
+        self.number = self.potential[self.pindex]
 
     def proceed_potential(self) -> bool:
-        if self.pindex + 1 < len(self.potentials):
+        if self.pindex + 1 < len(self.potential):
             self.pindex += 1
             self.lock_number()
             return True
@@ -28,3 +26,7 @@ class SudokuSquare:
             return True
         else:
             return False
+
+    def reset_square(self) -> None:
+        self.pindex = -1
+        self.number = 0
--- sudoku-cli/test/test_sudoku_square.py
+++ sudoku-cli/test/test_sudoku_square.py
@@ -1,5 +1,4 @@
 import pytest
-from typing import List
 from sudoku_square import SudokuSquare
 
 @pytest.fixture
@@ -13,16 +12,16 @@ def default_square():
 def common_square():
     row: int = 0
     col: int = 0
-    potentials: List = [1,2]
-    square = SudokuSquare(row, col, potentials)
+    potential: List = [1,2]
+    square = SudokuSquare(row, col, potential)
     return square
 
 @pytest.fixture
 def middle_square():
     row: int = 0
     col: int = 0
-    potentials: List = [1,2,3]
-    square = SudokuSquare(row, col, potentials)
+    potential: List = [1,2,3]
+    square = SudokuSquare(row, col, potential)
     square.proceed_potential()
     square.proceed_potential()
     return square
@@ -33,29 +32,27 @@ def test_default_square_class_initialized(default_square):
 def test_default_square_class_default_values(default_square):
     pindex: int = -1
     number: int = 0
-    solved: bool = False
-    potentials: List = []
+    potential: List = []
     assert default_square.pindex == pindex
     assert default_square.number == number
-    assert default_square.solved == solved
-    assert default_square.potentials == potentials
+    assert default_square.potential == potential
 
 def test_common_square_locks_number(common_square):
     pindex = 0
-    potentials = [1,2]
+    potential = [1,2]
     common_square.pindex = pindex
     common_square.lock_number()
-    assert common_square.number == potentials[pindex]
+    assert common_square.number == potential[pindex]
 
 def test_common_square_proceeds_potential(common_square):
-    potentials = [1,2]
+    potential = [1,2]
     assert common_square.number == 0
     assert common_square.proceed_potential()
-    assert common_square.number == potentials[0]
+    assert common_square.number == potential[0]
     assert common_square.proceed_potential()
-    assert common_square.number == potentials[1]
+    assert common_square.number == potential[1]
     assert not common_square.proceed_potential()
-    assert common_square.number == potentials[1]
+    assert common_square.number == potential[1]
 
 def test_default_square_not_proceeds_potential(default_square):
     assert default_square.number == 0
@@ -63,9 +60,16 @@ def test_default_square_not_proceeds_potential(default_square):
     assert default_square.number == 0
 
 def test_middle_square_precedes_potential(middle_square):
-    potentials = [1,2,3]
-    assert middle_square.number == potentials[1]
+    potential = [1,2,3]
+    assert middle_square.number == potential[1]
     assert middle_square.precede_potential()
-    assert middle_square.number == potentials[0]
+    assert middle_square.number == potential[0]
     assert not middle_square.precede_potential()
-    assert middle_square.number == potentials[0]
+    assert middle_square.number == potential[0]
+
+def test_middle_square_resets_square(middle_square):
+    pindex = -1
+    number = 0
+    middle_square.reset_square()
+    assert middle_square.pindex == pindex
+    assert middle_square.number == number
