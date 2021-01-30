945a04fee5bb9affcd92388ddb7429033c34297e
jesugq <jesugq@gmail.com>
2021-01-30 09:24:41 -0600

    Add basic backtrack functions, tests

--- sudoku-cli/sudoku_solver.py
+++ sudoku-cli/sudoku_solver.py
@@ -1,6 +1,43 @@
 from typing import List
 from sudoku_board import SudokuBoard
+from sudoku_square import SudokuSquare
 
 class SudokuSolver:
-    def __init__(self):
-        self.board: SudokuBoard()
+    def __init__(self, puzzle: List[List[int]] = None):
+        self.guide: int = -1
+        self.board: SudokuBoard = SudokuBoard(puzzle)
+        self.squares: List[SudokuSquare] = []
+        self.init_squares()
+
+    def init_squares(self):
+        for i, row in enumerate(self.board.puzzle):
+            for j, number in enumerate(row):
+                if number == 0:
+                    potentials = self.board.grab_potential_pos(i, j)
+                    self.squares.append(SudokuSquare(i, j, potentials))
+
+    def advance_guide(self) -> bool:
+        if self.guide + 1 < len(self.squares):
+            self.guide += 1
+            return True
+        else:
+            return False
+
+    def backtrack_guide(self) -> bool:
+        if self.guide -1 >= 0:
+            self.guide -= 1
+            return True
+        else:
+            return False
+    
+    def settle(self) -> bool:
+        board = self.board
+        square = self.squares[self.guide]
+        while square.proceed_potential():
+            board.edit_puzzle(square.number, square.row, square.col)
+            board.print_puzzle()
+            if board.check_pos(square.row, square.col):
+                return True
+        board.edit_puzzle(0, square.row, square.col)
+        square.reset_square()
+        return False
--- sudoku-cli/test/test_sudoku_solver.py
+++ sudoku-cli/test/test_sudoku_solver.py
@@ -1,11 +1,106 @@
 import pytest
-from typing import List
 from sudoku_solver import SudokuSolver
 
 @pytest.fixture
-def default_sudoku():
-    sudoku = SudokuSolver()
-    return sudoku
+def default_solver():
+    solver = SudokuSolver()
+    return solver
 
-def test_sudoku_initialized(default_sudoku):
-    assert default_sudoku
+@pytest.fixture
+def unsolved_solver(unsolved_puzzle):
+    solver = SudokuSolver(unsolved_puzzle)
+    return solver
+
+@pytest.fixture
+def first_solver():
+    solver = SudokuSolver()
+    solver.guide = 0
+    return solver
+
+@pytest.fixture
+def middle_solver():
+    solver = SudokuSolver()
+    solver.guide = 40
+    return solver
+
+@pytest.fixture
+def last_solver():
+    solver = SudokuSolver()
+    solver.guide = 80
+    return solver
+
+@pytest.fixture
+def unsettled_solver(unsettled_puzzle):
+    solver = SudokuSolver(unsettled_puzzle)
+    solver.guide = 0
+    return solver
+
+@pytest.fixture
+def unsettled_puzzle():
+    return [[1,2,3,4,5,6,7,8,0],
+            [0,0,0,0,0,0,0,0,9],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]]
+
+def test_default_solver_initialized(default_solver):
+    assert default_solver
+
+def test_default_solver_default_values(default_solver, empty_puzzle):
+    guide = -1
+    length = 81
+    potential = [1,2,3,4,5,6,7,8,9]
+    assert default_solver.guide == guide
+    assert default_solver.board.puzzle == empty_puzzle
+    assert len(default_solver.squares) == length
+    index = 0
+    for i in range(9):
+        for j in range(9):
+            assert default_solver.squares[index].row == i
+            assert default_solver.squares[index].col == j
+            assert default_solver.squares[index].potential == potential
+            index += 1
+
+def test_unsolved_solver_default_values(unsolved_solver, unsolved_puzzle):
+    assert unsolved_solver.board.puzzle == unsolved_puzzle
+    assert len(unsolved_solver.squares) == 53
+    first_index = 0
+    first_potential = [5,6,9]
+    last_index = 52
+    last_potential = [1,4,5,6]
+    assert unsolved_solver.squares[first_index].row == 0
+    assert unsolved_solver.squares[first_index].col == 0
+    assert unsolved_solver.squares[first_index].potential == first_potential
+    assert unsolved_solver.squares[last_index].row == 8
+    assert unsolved_solver.squares[last_index].col == 8
+    assert unsolved_solver.squares[last_index].potential == last_potential
+
+def test_middle_solver_advances_guide(middle_solver):
+    assert middle_solver.guide == 40
+    assert middle_solver.advance_guide()
+    assert middle_solver.guide == 41
+
+def test_last_solver_not_advances_guide(last_solver):
+    assert last_solver.guide == 80
+    assert not last_solver.advance_guide()
+    assert last_solver.guide == 80
+
+def test_middle_solver_backtracks_guide(middle_solver):
+    assert middle_solver.guide == 40
+    assert middle_solver.backtrack_guide()
+    assert middle_solver.guide == 39
+
+def test_first_solver_not_backtracks_guide(first_solver):
+    assert first_solver.guide == 0
+    assert not first_solver.backtrack_guide()
+    assert first_solver.guide == 0
+
+def test_first_solver_settles(first_solver):
+    assert first_solver.settle()
+
+def test_unsettled_solver_not_settles(unsettled_solver):
+    assert not unsettled_solver.settle()
